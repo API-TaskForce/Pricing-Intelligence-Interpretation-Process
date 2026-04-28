09dec5bcdaf67f3888b92abbb5c6a8e60a5d1d8b
Daniel Ruiz López <danielruiz13524@gmail.com>
2026-04-28 11:49:56 +0200

    feat: add constants for external datasheet URL endpoints

--- frontend/src/datasheets/index.ts
+++ frontend/src/datasheets/index.ts
@@ -1,14 +1,14 @@
 export const SENDGRID_2025_URL =
-  "https://datasheet-bucket.fra1.digitaloceanspaces.com/aiss-workshop/datasheets/Sendgrid@RAPIDAPI_datasheet-2025.yaml";
+  "http://165.245.246.76/static/datasheets/uploaded/sendgrid-rapidapi-2025/2026-04-19.yaml";
 
 export const SENDGRID_2026_URL =
-  "https://datasheet-bucket.fra1.digitaloceanspaces.com/aiss-workshop/datasheets/Sendgrid@RAPIDAPI_datasheet-2026.yaml";
+  "http://165.245.246.76/static/datasheets/uploaded/sendgrid-rapidapi-2026/2026-04-19.yaml";
 
 export const PEERTUBE_URL =
-  "http://194.62.96.89/static/datasheets/uploaded/peertube/2026-04-13.yaml";
+  "http://165.245.246.76/static/datasheets/uploaded/peertube/2026-04-13.yaml";
 
 export const DAILYMOTION_URL =
-  "http://194.62.96.89/static/datasheets/uploaded/dailymotion/2026-04-13.yaml";
+  "http://165.245.246.76/static/datasheets/uploaded/dailymotion/2026-04-13.yaml";
 
 export const MAILERSEND_URL =
-  "http://194.62.96.89/static/datasheets/uploaded/mailersend/2026-03-19.yaml";
+  "http://165.245.246.76/static/datasheets/uploaded/mailersend/2026-03-19.yaml";
